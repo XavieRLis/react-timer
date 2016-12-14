843a964716b329aaa2623bf224b64421d6a097e6
xavier.lis <xavier.lis@gmail.com>
2016-12-14 21:47:56 +0300

    init commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var Navigation = require('Navigation');
+
+var Main = (props) => (
+  <div>
+    <div>
+      <div>
+        <Navigation/>
+        <p>Main.jsx rendered</p>
+        {props.children}
+      </div>
+    </div>
+  </div>
+);
+
+module.exports = Main;
